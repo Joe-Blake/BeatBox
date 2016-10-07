3ca0f81ad5555690608ecc9efb343b954d60396f
Joe-Blake <wzj514332873@yahoo.com>
2016-10-08 00:15:19 +0800

    init

---
+++ app/src/main/java/com/example/joe/beatbox/SingleFragmentActivity.java
@@ -0,0 +1,10 @@
+package com.example.joe.beatbox;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by joe on 2016/10/8.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+}
